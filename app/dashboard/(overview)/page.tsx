e7d5046b36b9a4c460315e0236927a4d1721d715
Norio Suzuki <norio.suzuki@gmail.com>
2023-12-07 08:19:04 +0900

    Chapter 16

--- app/dashboard/(overview)/page.tsx
+++ app/dashboard/(overview)/page.tsx
@@ -5,6 +5,11 @@ import LatestInvoices from "@/app/ui/dashboard/latest-invoices";
 import CardWrapper, { Card } from "@/app/ui/dashboard/cards";
 import { Suspense } from "react";
 import { CardSkeleton, LatestInvoicesSkeleton, RevenueChartSkeleton } from "@/app/ui/skeletons";
+import { Metadata } from "next";
+
+export const metadata: Metadata = {
+  title: 'Dashboard',
+}
 
 export default async function Page() {
   const { numberOfCustomers, numberOfInvoices, totalPaidInvoices, totalPendingInvoices } = await fetchCardData();
